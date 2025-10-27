52b1375c54f74e4b4dbe4ee7c9cfb61f0d847c6b
Ale Pereira <naitwa-alexandre@gmail.com>
2025-10-27 11:40:12 -0300

    troll finite state machine example

--- State-Driven-Agent-Design/State-Driven-Agent-Design/Main.cpp
+++ State-Driven-Agent-Design/State-Driven-Agent-Design/Main.cpp
@@ -1,5 +1,81 @@
 #include <iostream>
 
+class State
+{
+public:
+	virtual void Execute(Troll* troll) = 0;
+};
+
+class Troll
+{
+	State* m_pCurrentState;
+
+public:
+
+	void Update()
+	{
+		m_pCurrentState->Execute(this);
+	}
+
+	void ChangeState(State* pNewState)
+	{
+		delete m_pCurrentState;
+		m_pCurrentState = pNewState;
+	}
+
+	bool isSafe()
+	{
+		return true;
+	}
+
+	void MoveAwayFromEnemy()
+	{
+		std::cout << "Moving Away from enemy\n";
+	}
+
+	bool isThreatened()
+	{
+		std::cout << "Feeling threatened\n";
+		return true;
+	}
+
+	void Snore()
+	{
+		std::cout << "Troll is snoring\n";
+	}
+};
+
+class State_RunAway : public State
+{
+	void Execute(Troll* troll)
+	{
+		if (troll->isSafe())
+		{
+			troll->ChangeState(new State_Sleep());
+		}
+		else
+		{
+			troll->MoveAwayFromEnemy();
+		}
+	}
+};
+
+class State_Sleep : public State
+{
+public:
+	void Execute(Troll* troll)
+	{
+		if (troll->isThreatened())
+		{
+			troll->ChangeState(new State_RunAway());
+		}
+		else
+		{
+			troll->Snore();
+		}
+	}
+};
+
 int main()
 {
 	std::cout << "Hello Bob the miner!" << std::endl;
