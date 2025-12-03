d0429bf6c9e1b391e7b9e937a85adf1efbff3d48
Ale Pereira <naitwa-alexandre@gmail.com>
2025-12-03 07:07:08 -0300

    first commit

---
+++ Autonomously-Moving-Game-Agents/Autonomously-Moving-Game-Agents/Src/Public/BaseGameEntity.cpp
@@ -0,0 +1,72 @@
+#ifndef BASEGAMEENTITY_H
+#define BASEGAMEENTITY_H
+#pragma warning (disable:4786)
+
+#include <vector>
+#include <string>
+#include <iosfwd>
+
+
+struct Vector2D
+{
+	float x,y;
+};
+
+class BaseGameEntity
+{
+public:
+
+	enum { default_entity_type = -1 };
+
+private:
+
+	static int  m_iNextValidID;
+
+	int         m_ID;
+
+	int         m_iType;
+
+	bool        m_bTag;
+
+	Vector2D    m_vPos;
+
+	double      m_dScale;
+
+	double      m_dBoundingRadius;
+
+	void SetID(int val);
+
+public:
+
+	BaseGameEntity(const int id)
+	{
+		SetID(id);
+	}
+	
+	virtual ~BaseGameEntity() = default;
+
+	// entities should be able to read/write data
+	virtual void  Write(std::ostream& os)const {}
+	virtual void  Read(std::ifstream& is) {}
+
+	Vector2D      Pos()const { return m_vPos; }
+	void          SetPos(Vector2D new_pos) { m_vPos = new_pos; }
+
+	double        BRadius()const { return m_dBoundingRadius; }
+	void          SetBRadius(double r) { m_dBoundingRadius = r; }
+
+	int           ID()const { return m_ID; }
+
+	bool          IsTagged()const { return m_bTag; }
+	void          Tag() { m_bTag = true; }
+	void          UnTag() { m_bTag = false; }
+
+	int           EntityType()const { return m_iType; }
+	void          SetEntityType(int new_type) { m_iType = new_type; }
+
+	double        Scale()const { return m_dScale; }
+	void          SetScale(double val) { m_dScale = val; }
+
+};
+
+#endif
