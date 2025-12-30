5a8e63c856fcbc2a0362f1fd409a178b60b1a190
Ale Pereira <naitwa-alexandre@gmail.com>
2025-12-30 09:34:48 -0300

    feat: creates hide behavior

--- Autonomously-Moving-Game-Agents/Autonomously-Moving-Game-Agents/Src/Private/SteeringBehaviors.cpp
+++ Autonomously-Moving-Game-Agents/Autonomously-Moving-Game-Agents/Src/Private/SteeringBehaviors.cpp
@@ -326,3 +326,55 @@ SVector2D SteeringBehaviors::Interpose(const Vehicle* AgentA, const Vehicle* Age
     //then steer to arrive at it
     return Arrive(MidPoint, EDeceleration::Fast);
 }
+
+SVector2D SteeringBehaviors::GetHidingPosition(const SVector2D& posOb, const double radiusOb, const SVector2D& posTarget)
+{
+    //calculate how far away the agent is to be from the chosen obtscle's
+    //bounding radius
+    const double DistanceFromBoundary = 30.0;
+
+    double DistAway = radiusOb + DistanceFromBoundary;
+
+    //calculate the heading toward the object from the target
+    SVector2D ToOb = SVector2D::Normalize(posOb - posTarget);
+
+    //scale it to size and add to the obstacle's position to get
+    //the hiding spot.
+    return (ToOb * DistAway) + posOb;
+}
+
+SVector2D SteeringBehaviors::Hide(const Vehicle* target, vector<BaseGameEntity*>& obstacles)
+{
+    double DistToClosest = MaxDouble;
+    SVector2D BestHidingSpot;
+
+    std::vector<BaseGameEntity*>::iterator curOb = obstacles.begin();
+
+    while (curOb != obstacles.end())
+    {
+        //calculate the position of the hiding spot for this obstacle
+        SVector2D HidingSpot = GetHidingPosition(
+            (*curOb)->Pos(), (*curOb)->BRadius(), target->Pos()
+        );
+
+        //work in distance-squared space to find  the closest hiding
+        //spot to the agent
+        double dist = SVector2D::DistanceSquared(HidingSpot, m_pVehicle->Pos());
+
+        if (dist < DistToClosest)
+        {
+            DistToClosest = dist;
+            BestHidingSpot = HidingSpot;
+        }
+
+        ++curOb;
+    }
+
+    //if no suitable obstacles found then evade the target
+    if (DistToClosest == MaxDouble)
+    {
+        return Evade(target);
+    }
+
+    return Arrive(BestHidingSpot, EDeceleration::Fast);
+}
