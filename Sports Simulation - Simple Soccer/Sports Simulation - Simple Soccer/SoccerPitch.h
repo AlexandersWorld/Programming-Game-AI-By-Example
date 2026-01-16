dbf814503c456928c820e5391a7df5952aa25ca5
Ale Pereira <naitwa-alexandre@gmail.com>
2026-01-16 08:30:14 -0300

    feat: implement soccerpitch class and understand how it works as a soccer field in game

--- Sports Simulation - Simple Soccer/Sports Simulation - Simple Soccer/SoccerPitch.h
+++ Sports Simulation - Simple Soccer/Sports Simulation - Simple Soccer/SoccerPitch.h
@@ -1,11 +1,35 @@
 #pragma once
+#include "SoccerTeam.h"
+#include "Goal.h"
+#include "SoccerBall.h"
+#include "Wall2D.h"
+#include "Region.h"
 
 class SoccerPitch
 {
 
 public:
-	SoccerPitch();
+	SoccerPitch(int cxClient, int cyClient);
+
+	~SoccerPitch();
 
 	void Update();
 	void Render();
+
+private:
+	SoccerTeam* m_pBlueTeam;
+	SoccerTeam* m_pRedTeam;
+	SoccerBall* m_pSoccerBall;
+	Goal* m_pRedGoal;
+	Goal* m_pBlueGoal;
+
+	std::vector<Wall2D> m_vecWallks;
+	std::vector<Region*> m_Regions;
+
+	bool m_bGameOn;
+	bool m_bGoalKeeperHasBall;
+
+	int m_cxClient;
+	int m_cyClient;
+
 };
